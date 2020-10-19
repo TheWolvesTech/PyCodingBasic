a2356248eff99d579640133e6e269cc3f5b54c9e
TheWolvesTech <73081191+TheWolvesTech@users.noreply.github.com>
2020-10-19 10:42:25 -0400

    Update practice6.py

--- practice6.py
+++ practice6.py
@@ -1,4 +1,6 @@
 """
+TRY IT YOURSELF
+
 3-8. Seeing the World: Think of at least five places in the world you’d like to
 visit.
 Store the locations in a list. Make sure the list is not in alphabetical order.
