51efa0c9a578bf2af73b360593c9ee61cdf5f42e
TheWolvesTech <73081191+TheWolvesTech@users.noreply.github.com>
2020-10-19 10:41:53 -0400

    Update practice5.py

--- practice5.py
+++ practice5.py
@@ -1,4 +1,6 @@
 """
+TRY IT YOURSELF
+
 3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who
 would you invite? Make a list that includes at least three people you’d like to
 invite to dinner. Then use your list to print a message to each person, inviting
