7bbe49bc3bbbdda18e85bf10b647e9096a0d8389
TheWolvesTech <73081191+TheWolvesTech@users.noreply.github.com>
2020-10-19 20:36:17 -0400

    Add files via upload

---
+++ practice17.py
@@ -0,0 +1,16 @@
+"""
+6-1. Person: Use a dictionary to store information about a person you know.
+Store their first name, last name, age, and the city in which they live. You
+should have keys such as first_name, last_name, age, and city. Print each
+piece of information stored in your dictionary.
+
+"""
+#6-1
+person_info = {"name": "Taylor","last_name": "McKnigt","age": "24",
+"city": "NY"
+}   #Dicctionary definition.
+print("Personal information.")
+print("Name :", person_info['name'])
+print("Last name :", person_info["last_name"])
+print("Age : ", person_info["age"],"Years Old.")
+print("City :", person_info["city"])
